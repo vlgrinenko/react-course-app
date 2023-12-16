0ed29144af8dce99b759c8fe2442336faf199abf
vladimir.grinenko <S1vjycnhbr>
2023-12-16 23:29:42 +0300

    feat: add Costs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import CostItem from './components/CostItem';
+import Costs from './components/Costs';
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: 'Холодильник',
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: 'MacBook',
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 3, 1),
+      description: 'Джинсы',
+      amount: 49.99,
+    },
+  ];
+
   return (
     <div>
       <h1>Начнем изучение React!</h1>
-      <CostItem></CostItem>
+      <Costs costs={costs} />
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,14 +1,16 @@
 import './CostItem.css';
+import CostDate from './CostDate';
+import Card from './Card';
 
-function CostItem() {
+function CostItem(props) {
   return (
-    <div className='cost-item'>
-      <div>март 2021 12</div>
+    <Card className='cost-item'>
+      <CostDate date={props.date} />
       <div className='cost-item__description'>
-        <h2>Холодильник</h2>
-        <div className='cost-item__price'>$999.99</div>
+        <h2>{props.description}</h2>
+        <div className='cost-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 

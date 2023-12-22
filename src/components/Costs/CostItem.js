54e8774c6200841765ff52466a5775d45307e839
vladimir.grinenko <S1vjycnhbr>
2023-12-22 13:18:48 +0300

    fix: delete button in CostItem

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,22 +1,15 @@
 import './CostItem.css';
 import CostDate from './CostDate';
 import Card from '../UI/Card';
-import React, { useState } from 'react';
 
 const CostItem = (props) => {
-  const [description, setDescription] = useState(props.description);
-
-  const changeDescriptionHandler = () => {
-    setDescription('New text');
-  };
   return (
     <Card className='cost-item'>
       <CostDate date={props.date} />
       <div className='cost-item__description'>
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className='cost-item__price'>${props.amount}</div>
       </div>
-      <button onClick={changeDescriptionHandler}>Изменить описание</button>
     </Card>
   );
 };
